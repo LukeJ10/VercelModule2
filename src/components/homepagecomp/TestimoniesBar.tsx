2c0c4e94c3e602e9fad8017fe1843000b8ff9579
LukeJ10 <lukas.jiun@gmail.com>
2024-06-19 17:38:55 +0700

    update

--- src/components/homepagecomp/TestimoniesBar.tsx
+++ src/components/homepagecomp/TestimoniesBar.tsx
@@ -1,73 +1,82 @@
 import React from "react";
+import Image from 'next/image';
 
 const TestimoniesBar = () => {
   return (
     <div className="bg-white py-16 min-h-[600px]">
-      <h2 className="text-center text-3xl  text-black font-bold mb-12">
+      <h2 className="text-center text-3xl text-black font-bold mb-12">
         Testimonies
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-7 max-w-6xl mx-auto bg-gray-200">
         <div className="bg-gray-300 rounded-lg p-3 flex">
-          <div className="w-1/4  flex-shrink-0 ">
-            <img
+          <div className="w-1/4 flex-shrink-0">
+            <Image
               src="/images/tesA.png"
               alt="testimoni satu"
-              className="object-cover "
+              width={200}
+              height={200}
+              className="object-cover"
             />
           </div>
           <div className="w-3/4 ml-4">
             <p className="text-sm leading-relaxed text-black">
-              "Arasaki Games Studio has consistently delivered top-notch gaming
+              &quot;Arasaki Games Studio has consistently delivered top-notch gaming
               experiences that exceed our expectations. Their creativity and
-              attention to detail are unparalleled."
+              attention to detail are unparalleled.&quot;
             </p>
           </div>
         </div>
-        <div className="bg-gray-300 rounded-lg p-3  flex">
-          <div className="w-1/4  flex-shrink-0 ">
-            <img
+        <div className="bg-gray-300 rounded-lg p-3 flex">
+          <div className="w-1/4 flex-shrink-0">
+            <Image
               src="/images/tesB.png"
               alt="testimoni Dua"
-              className="object-cover "
+              width={200}
+              height={200}
+              className="object-cover"
             />
           </div>
           <div className="w-3/4 ml-4">
             <p className="text-sm leading-relaxed text-black">
-              "Working with Arasaki Games Studio has been a game-changer for us.
+              &quot;Working with Arasaki Games Studio has been a game-changer for us.
               Their innovative approach and dedication to excellence set them
-              apart from the competition."
+              apart from the competition.&quot;
             </p>
           </div>
         </div>
-        <div className="bg-gray-300 rounded-lg p-3  flex">
-          <div className="w-1/4  flex-shrink-0 ">
-            <img
+        <div className="bg-gray-300 rounded-lg p-3 flex">
+          <div className="w-1/4 flex-shrink-0">
+            <Image
               src="/images/tesC.png"
               alt="testimoni Tiga"
-              className="object-cover "
+              width={200}
+              height={200}
+              className="object-cover"
             />
           </div>
           <div className="w-3/4 ml-4">
             <p className="text-sm leading-relaxed text-black">
-              "The team at Arasaki Games Studio is incredibly talented and
+              &quot;The team at Arasaki Games Studio is incredibly talented and
               professional. Their games are not only visually stunning but also
-              highly engaging."
+              highly engaging.&quot;
             </p>
           </div>
         </div>
-        <div className="bg-gray-300 rounded-lg p-3  flex">
-          <div className="w-1/4  flex-shrink-0 ">
-            <img
+        <div className="bg-gray-300 rounded-lg p-3 flex">
+          <div className="w-1/4 flex-shrink-0">
+            <Image
               src="/images/tesD.png"
               alt="testimoni empat"
-              className="object-cover "
+              width={200}
+              height={200}
+              className="object-cover"
             />
           </div>
           <div className="w-3/4 ml-4">
             <p className="text-sm leading-relaxed text-black">
-              "Arasaki Games Studio's commitment to quality and innovation is
+              &quot;Arasaki Games Studio&apos;s commitment to quality and innovation is
               evident in every project they undertake. We highly recommend
-              them."
+              them.&quot;
             </p>
           </div>
         </div>
